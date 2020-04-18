ec07eaa1e704f362f6af7722922d7d455c653781
ambsenyestudi <adrifre7@hotmail.com>
2020-04-18 19:22:39 +0200

    Trip service test red fase. Lots of dependencies

--- src/TripServiceKata/TripServiceKata.Domain/Trips/TripService.cs
+++ src/TripServiceKata/TripServiceKata.Domain/Trips/TripService.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using TripServiceKata.Domain.Exceptions;
 using TripServiceKata.Domain.Users;
 
---
+++ src/TripServiceKata/TripServiceKata.Test/TripServiceTests/TripServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using TripServiceKata.Domain.Users;
+using TripServiceKata.Domain.Trips;
+using Xunit;
+using TripServiceKata.Domain.Exceptions;
+
+namespace TripServiceKata.Test.TripServiceTests
+{
+    public class trip_service_should
+    {
+        [Fact]
+        public void kick_out_when_user_not_logged_in()
+        {
+            var someUser = new User();
+            var tripService = new TripService();
+
+            Assert.Throws<UserNotLoggedInException>(()=>tripService.GetTripsByUser(someUser));
+        }
+    }
+}
