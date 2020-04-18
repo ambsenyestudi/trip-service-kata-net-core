75195b4e5c6d13817ecdb1a0ec4c6c0b1d7733c3
ambsenyestudi <adrifre7@hotmail.com>
2020-04-18 19:17:05 +0200

    Trip Service added

---
+++ src/TripServiceKata/TripServiceKata.Domain/Trips/TripService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TripServiceKata.Domain.Exceptions;
+using TripServiceKata.Domain.Users;
+
+namespace TripServiceKata.Domain.Trips
+{
+    public class TripService
+    {
+        public List<Trip> GetTripsByUser(User user)
+        {
+            List<Trip> tripList = new List<Trip>();
+            User loggedUser = UserSession.GetInstance().GetLoggedUser();
+            bool isFriend = false;
+            if (loggedUser != null)
+            {
+                foreach (User friend in user.GetFriends())
+                {
+                    if (friend.Equals(loggedUser))
+                    {
+                        isFriend = true;
+                        break;
+                    }
+                }
+                if (isFriend)
+                {
+                    tripList = TripDAO.FindTripsByUser(user);
+                }
+                return tripList;
+            }
+            else
+            {
+                throw new UserNotLoggedInException();
+            }
+        }
+    }
+}
